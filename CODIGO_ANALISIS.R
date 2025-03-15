1aacca4fc5e15249a0b8619257f67e6e6d9736f4
RcampoR <rcamporomero88@correo.unicordoba.edu.co>
2025-03-14 23:30:26 -0500

    añadir texto R

---
+++ CODIGO_ANALISIS.R
@@ -0,0 +1,7 @@
+library(readr)
+library(tibble)
+library(dplyr)
+
+file.choose()
+
+BASE_ORIGINAL <- read_delim("C:\\Proyectos Actuales\\TAREA TRACHEOPHYTAS\\bases de datos\\DATOS_TRACHEOPHYTA_COLOMBIA\\tracheophyta_colombia.csv")
