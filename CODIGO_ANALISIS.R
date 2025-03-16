a6d11f0e726fdc0c506ae8874da04914d2ed66d6
RcampoR <rcamporomero88@correo.unicordoba.edu.co>
2025-03-15 23:20:21 -0500

    guardar

--- CODIGO_ANALISIS.R
+++ CODIGO_ANALISIS.R
@@ -1,7 +1,40 @@
 library(readr)
 library(tibble)
 library(dplyr)
+library(ggplot2)
+library(tidyverse)
 
 file.choose()
 
 BASE_ORIGINAL <- read_delim("C:\\Proyectos Actuales\\TAREA TRACHEOPHYTAS\\bases de datos\\DATOS_TRACHEOPHYTA_COLOMBIA\\tracheophyta_colombia.csv")
+
+
+# NUMERO DE ESPECIES DE PLANTAS VASCULARES EN COLOMBIA 
+BASE_ORIGINAL %>%
+  filter(!is.na(species)) %>% 
+  summarise(Total_Colombia = n_distinct(species))
+
+
+#NUMERO DE ESPECIES POR CLASE
+
+clases_riqueza <- BASE_ORIGINAL %>% 
+ filter(!is.na(class)) %>% 
+  group_by(class) %>% 
+  summarise(n_especies = n_distinct(species, na.rm = TRUE)) 
+
+
+
+clases_riqueza %>% 
+  pivot_wider(names_from = class,
+              values_from = n_especies) %>% 
+  ggplot() +
+  geom_bar(aes(Cycadopsida:Magnoliopsida))
+  
+
+clases_riqueza %>% 
+  pivot_wider(names_from = class,
+              values_from = n_especies) %>% 
+  ggplot() +
+  geom_bar(aes(Cycadopsida:Magnoliopsida))
+
+
